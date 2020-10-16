cf366289d9b980a4a9a83560ea3fc5a16b91ae0c
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-16 14:03:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,8 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
   output = ""
-  if songs.include?
+  if songs.include?(input)
+    output << "Playing #{"
   puts output
 end
 
